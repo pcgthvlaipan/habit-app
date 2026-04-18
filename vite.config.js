74bf21798148e2f20b4fad8fc00baa8e2ad62d95
Tam atwork <pcgthvlaipan@gmail.com>
2026-04-18 14:56:16 +0000

    Fix iOS Safari with esbuild target

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: ["es2015", "safari11"],
+    target: "es2015",
+    minify: "esbuild",
+  },
+  esbuild: {
+    target: "es2015",
   },
 });
