3533dc9fca77036a2d7e43337d5b102c6b7fcb3f
pcgthvlaipan <pcgthvlaipan@gmail.com>
2026-05-18 12:43:53 +0000

    Add legacy plugin for iOS Safari support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["ios >= 13", "safari >= 13"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
   build: {
     target: "es2015",
     minify: "esbuild",
